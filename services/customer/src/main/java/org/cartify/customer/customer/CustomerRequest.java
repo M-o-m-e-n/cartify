0b5b158b7b786f3154901bb3dc2b41eda932ed4b
Momen <momenmostafa203@gmail.com>
2026-05-06 14:47:54 +0300

    Initial commit

---
+++ services/customer/src/main/java/org/cartify/customer/customer/CustomerRequest.java
@@ -0,0 +1,7 @@
+package org.cartify.customer.customer;
+
+import lombok.Data;
+
+@Data
+public record CustomerRequest {
+}
---
+++ services/customer/src/main/java/org/cartify/customer/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package org.cartify.customer.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+}
