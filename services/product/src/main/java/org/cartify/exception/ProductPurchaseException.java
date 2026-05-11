568ee2a3e0c390216f5cdd3b5e3e80ce139841ab
Momen <momenmostafa203@gmail.com>
2026-05-11 18:29:39 +0300

    Add global exception handling for product purchase and validation errors

---
+++ services/product/src/main/java/org/cartify/exception/ProductPurchaseException.java
@@ -0,0 +1,8 @@
+package org.cartify.exception;
+
+public class ProductPurchaseException extends RuntimeException {
+    public ProductPurchaseException(String message) {
+        super(message);
+    }
+}
+
---
+++ services/product/src/main/java/org/cartify/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package org.cartify.handler;
+
+import java.util.Map;
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {
+
+}
