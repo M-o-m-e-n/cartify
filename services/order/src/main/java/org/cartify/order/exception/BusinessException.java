77f3dcafd6f1526e724f3b40d2eab187a22b2525
Momen <momenmostafa203@gmail.com>
2026-05-13 17:20:04 +0300

    Add BusinessException class for handling business logic errors

---
+++ services/order/src/main/java/org/cartify/order/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package org.cartify.order.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class BusinessException extends RuntimeException {
+    private final String message;
+}
